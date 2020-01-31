9455304f7505dfb65d223b1e369d0cff51fb976f
Endre Sukosd <endre@spotlab.org>
2020-01-31 19:55:11 +0100

    Remove start comment, not useful

--- src/fast_forward_action.ts
+++ src/fast_forward_action.ts
@@ -8,9 +8,7 @@ export class FastForwardAction{
 
   async execute(client: GitHubClient, successMessage: string, failureMessage: string, closePRWhenFailed: boolean): Promise<void>{
     const pr_number = client.get_current_pull_request_number();
-
-    await client.comment_on_pull_request_async(pr_number, "Action Execution Started!");
-
+    
     try{
       await client.fast_forward_target_to_source_async(pr_number);
     } catch(error){
